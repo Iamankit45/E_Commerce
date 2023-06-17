5c4bcaef72bc6c517540ced0d9c6ecc747667af2
ANKIT <kumarankit0327@gmail.com>
2023-06-17 13:18:59 +0530

    local routes added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import React from "react";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Products from "./Products";
+import Cart from "./Cart";
+import Contact from "./Contact";
+import SingleProduct from "./SingleProduct";
+import ErrorPage from "./ErrorPage";
+
 const App = () => {
-  return <div>Welcome to Ankit React E-Commerce Website</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+
+        <Route path="/about" element={<About />} />
+        <Route path="/products" element={<Products/>} />
+        <Route path="/cart" element={<Cart/>} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/singleproduct/:id" element={<SingleProduct />} />
+        <Route path="*" element ={<ErrorPage/>}/>
+
+      </Routes>
+    </Router>
+
+
+
+
+
+  );
 };
 
 export default App;
