6c86081b91d587813ea466826c827dbd7755ae90
ANKIT <kumarankit0327@gmail.com>
2023-06-22 01:23:55 +0530

    Footer added

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ErrorPage from "./ErrorPage";
 import { GlobalStyle } from "./GlobalStyle";
 import { ThemeProvider } from "styled-components";
 import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 const App = () => {
 
@@ -57,7 +58,7 @@ const App = () => {
         <Route path="*" element ={<ErrorPage/>}/>
 
       </Routes>
-    
+    <Footer/>
     </Router>
 
 </ThemeProvider>
