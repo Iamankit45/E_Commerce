019f179cc4ba1872d2b7a8dee2ddf1e66d509423
ANKIT <kumarankit0327@gmail.com>
2023-06-21 23:52:42 +0530

    Trusted and services added

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,24 @@
 import React from "react";
 import styled from "styled-components";
 import HeroSection from "./Components/HeroSection";
+import Trusted from "./Components/Trusted";
 
+import Services from "./Components/Services";
 
 const Home=()=>{
 
   const data={
     name:"Ankit store",
   }
-return <HeroSection myData={data}/>
+return <>
+<HeroSection myData={data}/>
 
-
+<Services/>
+<Trusted/>
+</>
 }
+
+
 
 
 ;
