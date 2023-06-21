50469a949fcf2e650398e9f70bd0bb98c3e4d29f
ANKIT <kumarankit0327@gmail.com>
2023-06-22 00:39:00 +0530

    contact page added

--- src/Contact.js
+++ src/Contact.js
@@ -1,44 +1,89 @@
 import styled from "styled-components";
 
 const Contact = () => {
-  // const Wrapper = styled.section`
-  //   padding: 9rem 0 5rem 0;
-  //   text-align: center;
+  const Wrapper = styled.section`
+    padding: 9rem 0 5rem 0;
+    text-align: center;
 
-  //   .container {
-  //     margin-top: 6rem;
+    .container {
+      margin-top: 6rem;
 
-  //     .contact-form {
-  //       max-width: 50rem;
-  //       margin: auto;
+      .contact-form {
+        max-width: 50rem;
+        margin: auto;
 
-  //       .contact-inputs {
-  //         display: flex;
-  //         flex-direction: column;
-  //         gap: 3rem;
+        .contact-inputs {
+          display: flex;
+          flex-direction: column;
+          gap: 3rem;
 
-  //         input[type="submit"] {
-  //           cursor: pointer;
-  //           transition: all 0.2s;
+          input[type="submit"] {
+            cursor: pointer;
+            transition: all 0.2s;
 
-  //           &:hover {
-  //             background-color: ${({ theme }) => theme.colors.white};
-  //             border: 1px solid ${({ theme }) => theme.colors.btn};
-  //             color: ${({ theme }) => theme.colors.btn};
-  //             transform: scale(0.9);
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `;
+            &:hover {
+              background-color: ${({ theme }) => theme.colors.white};
+              border: 1px solid ${({ theme }) => theme.colors.btn};
+              color: ${({ theme }) => theme.colors.btn};
+              transform: scale(0.9);
+            }
+          }
+        }
+      }
+    }
+  `;
 
-  // return <Wrapper>
 
-  //   <h1>cpntact page</h1>
-  // </Wrapper>;
 
-  return <div> Contact</div>
+  return (
+    <Wrapper>
+
+      <h2 className="common-heading">Contact page</h2>
+
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3668.0373216724424!2d79.93095907529401!3d23.16883791085414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3981ae11af830313%3A0xf71864108d2a35ba!2sSamdariya%20Mall!5e0!3m2!1sen!2sin!4v1687373772720!5m2!1sen!2sin" 
+      width="100%"
+       height="400" 
+       style={{border:0 }}
+       allowFullScreen="" 
+       loading="lazy" 
+       referrerPolicy="no-referrer-when-downgrade">
+
+       </iframe>
+       <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/xayzrgok"
+            method="POST"
+            className="contact-inputs">
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              autoComplete="off"
+              required
+            />
+
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter you message"></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+        </div>
+    </Wrapper>);
 };
 
 export default Contact;
