fd1d1950f7f6f41d41d6277207c53a6f753a1979
ANKIT <kumarankit0327@gmail.com>
2023-06-23 22:58:26 +0530

    fetch API data using Axios

--- src/Context/productcontex.js
+++ src/Context/productcontex.js
@@ -3,17 +3,40 @@
 //consumer => use context hook
 
 
-import { createContext,useContext } from "react";
-
+import { createContext,useContext,useEffect } from "react";
+import axios from "axios";
 
 
 const AppContext=createContext();
+const API="https://api.pujakaitem.com/api/products";
+
 
 
 const AppProvider=({children})=>{
 
+
+const getProducts= async (url)=>{
+
+const res= await axios.get(url);
+const product =await res.data;
+
+}
+
+
+
+
+useEffect(()=>{
+getProducts(API);
+},[])
+
+
+
+
     return <AppContext.Provider value={{myName:"Ankit"}}>{children}</AppContext.Provider>
 }
+
+
+
 
 // custom hooks
 const useProductContext = () => {
