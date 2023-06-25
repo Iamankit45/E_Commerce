737b09cc451ed499d6707daa914c07d49e2bfc1d
ANKIT <kumarankit0327@gmail.com>
2023-06-25 18:12:33 +0530

    context API for singleProduct

--- src/Context/productcontex.js
+++ src/Context/productcontex.js
@@ -16,6 +16,8 @@ const initialState = {
   isError: false,
   products: [],
   featureProducts: [],
+  isSingleLoading: false,
+  singleProduct: {},
 };
 
 const AppProvider = ({ children }) => {
@@ -31,6 +33,25 @@ const AppProvider = ({ children }) => {
       dispatch({ type: "API_ERROR" });
     }
   };
+  
+  
+  const getSingleProduct=async(url)=>{
+
+
+    dispatch({ type: "SET_LOADING" });
+    try {
+      const res = await axios.get(url);
+      const singleProduct = await res.data;
+      dispatch({ type: "SET_SINGLE_PRODUCT",payload: singleProduct });
+    } catch (error) {
+      dispatch({ type: "SET_SINGLE_ERROR" });
+    }
+  }
+
+
+
+
+
 
   useEffect(() => {
     getProducts(API);
