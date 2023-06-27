d3ae77ffe6200a46e13c3d9d83cc1e43027aa4f0
ANKIT <kumarankit0327@gmail.com>
2023-06-27 20:16:35 +0530

    listView added

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -1,7 +1,7 @@
 
 import { useFilterContext } from "../Context/filterContext";
 import GridView from "./GridView";
-
+import ListView from "./ListView";
 const ProductList = () => {
 
     const { filter_products, grid_view } = useFilterContext();
@@ -11,6 +11,9 @@ const ProductList = () => {
         return <GridView products={filter_products} />;
     }
 
+    if (grid_view === false) {
+        return <ListView products={filter_products} />;
+    }
     return (<div>product</div>)
 }
 
