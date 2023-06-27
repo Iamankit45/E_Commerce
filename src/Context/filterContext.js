35f16e83e354ed935fe9862e2854cbd7aacb0c68
ANKIT <kumarankit0327@gmail.com>
2023-06-27 22:56:44 +0530

    sorting a-z,z-a,highest,lowest

--- src/Context/filterContext.js
+++ src/Context/filterContext.js
@@ -8,6 +8,7 @@ const initialState={
     filter_products:[],
     all_products:[],
     grid_view:false,
+    sorting_value:"lowest"
 
 };
 
@@ -20,17 +21,33 @@ export const FilterContextProvider = ({ children }) => {
 
    //to set the grid view
    const setGridView=()=>{
-    return dispatch({type:"SET_GRIDVIEW"})
+    return dispatch({type:"SET_GRID_VIEW"})
    }
 
-
+    //to set the list view
+   const setListView=()=>{
+    return dispatch({type:"SET_LIST_VIEW"})
+   }
     useEffect(()=>{
       dispatch({type:"LOAD_FILTER_PRODUCTS",payload:products})
 
     },[products])
 
 
-    return (<FilterContext.Provider value={{ ...state ,setGridView}}>{children}</FilterContext.Provider>);
+    // sorting function
+  const sorting = (event) => {
+    let userValue = event.target.value;
+    dispatch({ type: "GET_SORT_VALUE", payload: userValue });
+  };
+
+
+useEffect(()=>{
+
+   dispatch({type:"SORTING_PRODUCTS",payload:products})
+},[state.sorting_value]);
+
+
+    return (<FilterContext.Provider value={{ ...state ,setGridView,setListView,sorting}}>{children}</FilterContext.Provider>);
 };
 
 //custom hook bana rhe h
